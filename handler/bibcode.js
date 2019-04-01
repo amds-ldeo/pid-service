e8b09d1b36d012c68e75a1af01b569c565598340
Peng <spark.ji@gmail.com>
2019-04-01 02:10:08 -0400

    add fuction get bibcode by doi

--- handler/bibcode.js
+++ handler/bibcode.js
@@ -36,11 +36,43 @@ function getRefByBibcode(req, res, next) {
       });        
 }
 
+//Get metadata of the reference by DOI
+function getRefByDoi(req, res, next) {
+    let requestUrl = process.env.ADS_BIBCODE_API_BASE_URL + process.env.ADS_API_DOI_QUERY
+     + encodeURIComponent(req.params.doi) + process.env.ADS_API_FIELDS_RETURN;
+    let requestOpt = {
+        url: requestUrl,
+        auth: {
+            'bearer': process.env.ADS_API_KEY
+        }
+
+    }
+    request(requestOpt, function (error, response, body) {
+        if (response.statusCode == 200) {
+            let result = {};
+            let parseResult = parser.parseBIBCODE(JSON.parse(body));
+            if(!!parseResult && Object.keys(parseResult).length>0){
+                Object.assign(result,{status: "ok", count: 1, data: parseResult});
+            } else {
+                Object.assign(result,{status: "fail", count: 0});
+            }
+            res.status(200)
+                .json(result);
+        } else if (response.statusCode == 404) {
+            res.status(404)
+            .send("Resource not found.");
+        } else {
+            res.status(response.statusCode)
+            .send(error);
+        }
+      });        
+}
 
 
 
 
 
 module.exports = {
-    getRefByBibcode: getRefByBibcode
+    getRefByBibcode: getRefByBibcode,
+    getRefByDoi: getRefByDoi
 };
--- routes/bibcode.js
+++ routes/bibcode.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var handler = require('../handler/bibcode');
 
-/* GET users listing. */
+/* GET ref by bibcode */
 router.get('/:bibcode', handler.getRefByBibcode);
+
+/* GET ref by doi */
+router.get('/bydoi/:doi(*\/*)', handler.getRefByDoi);
 
 module.exports = router;
