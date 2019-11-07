041722821672775721c08942f1c4d779646bff13
pengji <spark.ji@gmail.com>
2019-11-07 15:49:56 -0500

    change error return structure

--- handler/bibcode.js
+++ handler/bibcode.js
@@ -17,25 +17,48 @@ function getRefByBibcode(req, res, next) {
 
     }
     request(requestOpt, function (error, response, body) {
+        let result = {};
+        let newStatusCode = null;
         if (response.statusCode == 200) {
-            let result = {};
+            //let result = {};
             let parseResult = parser.parseBIBCODE(JSON.parse(body));
             if(!!parseResult && Object.keys(parseResult).length>0){
                 Object.assign(result,{data: parseResult});
+                newStatusCode = 200;
             } else {
                 //Object.assign(result,{status: "fail", count: 0});
-                res.status(204)
-                .send("No content.");
+                let errors=[];
+                let errorEle = {};
+                Object.assign(errorEle,{status: "204", title: "The related content does not exist although the resource exists."});
+                errors.push(errorEle);
+                Object.assign(result,{errors:errors});
+                newStatusCode = 204;
+               // res.status(204)
+               // .send("No content.");
             }
-            res.status(200)
-                .json(result);
+            //res.status(200)
+            //    .json(result);
         } else if (response.statusCode == 404) {
-            res.status(404)
-            .send("Resource not found.");
+            let errors=[];
+            let errorEle = {};
+            Object.assign(errorEle,{status: "404", title: "The resource does not exist."});
+            errors.push(errorEle);
+            Object.assign(result,{errors:errors});
+            newStatusCode = 404;
+           // res.status(404)
+          //  .send("Resource not found.");
         } else {
-            res.status(response.statusCode)
-            .send(error);
+            let errors=[];
+            let errorEle = {};
+            Object.assign(errorEle,{status: response.statusCode.toString(), title: error});
+            errors.push(errorEle);
+            Object.assign(result,{errors:errors});
+            newStatusCode = response.statusCode;
+           // res.status(response.statusCode)
+            //.send(error);
         }
+        res.status(newStatusCode)
+        .json(result);
       });        
 }
 
@@ -51,24 +74,49 @@ function getRefByDoi(req, res, next) {
         }
 
     }
+
     request(requestOpt, function (error, response, body) {
+        let result = {};
+        let newStatusCode = null;
         if (response.statusCode == 200) {
-            let result = {};
+            //let result = {};
             let parseResult = parser.parseBIBCODE(JSON.parse(body));
             if(!!parseResult && Object.keys(parseResult).length>0){
-                Object.assign(result,{status: "ok", count: 1, data: parseResult});
+                //Object.assign(result,{status: "ok", count: 1, data: parseResult});
+                Object.assign(result,{data: parseResult});
+                newStatusCode = 200;
             } else {
-                Object.assign(result,{status: "fail", count: 0});
+                let errors=[];
+                let errorEle = {};
+                Object.assign(errorEle,{status: "204", title: "The related content does not exist although the resource exists."});
+                errors.push(errorEle);
+                Object.assign(result,{errors:errors});
+                newStatusCode = 204;
+                //Object.assign(result,{status: "fail", count: 0});
             }
-            res.status(200)
-                .json(result);
+            //res.status(200)
+            //    .json(result);
         } else if (response.statusCode == 404) {
-            res.status(404)
-            .send("Resource not found.");
+            let errors=[];
+            let errorEle = {};
+            Object.assign(errorEle,{status: "404", title: "The resource does not exist."});
+            errors.push(errorEle);
+            Object.assign(result,{errors:errors});
+            newStatusCode = 404;
+           // res.status(404)
+          //  .send("Resource not found.");
         } else {
-            res.status(response.statusCode)
-            .send(error);
+            let errors=[];
+            let errorEle = {};
+            Object.assign(errorEle,{status: response.statusCode.toString(), title: error});
+            errors.push(errorEle);
+            Object.assign(result,{errors:errors});
+            newStatusCode = response.statusCode;
+           // res.status(response.statusCode)
+          //  .send(error);
         }
+        res.status(newStatusCode)
+        .json(result);
       });        
 }
 
--- handler/crossrefDOI.js
+++ handler/crossrefDOI.js
@@ -8,25 +8,51 @@ function getRefByDoi(req, res, next) {
 
     let requestUrl = process.env.CROSSREF_API_BASE_URL + req.params.doi;
     request(requestUrl, function (error, response, body) {
+        let result = {};
+        let newStatusCode = null;
         if (response.statusCode == 200) {
-            let result = {};
+            
             let parseResult = parser.parseDOI(JSON.parse(body));
             if(!!parseResult && Object.keys(parseResult).length>0){
                 Object.assign(result,{data: parseResult});
+                newStatusCode = 200;
             } else {
                 //Object.assign(result,{status: "fail", count: 0});
-                res.status(204)
-                .send("No content.");
+                let errors=[];
+                let errorEle = {};
+                Object.assign(errorEle,{status: "204", title: "The related content does not exist although the resource exists."});
+                errors.push(errorEle);
+                Object.assign(result,{errors:errors});
+                newStatusCode = 204;
+
+               // res.status(204)
+                //.json(result);
+                //.send("No content.");
             }
-            res.status(200)
-                .json(result);
+            //res.status(200)
+             //   .json(result);
         } else if (response.statusCode == 404) {
-            res.status(404)
-            .send("Resource not found.");
+            let errors=[];
+            let errorEle = {};
+            Object.assign(errorEle,{status: "404", title: "The resource does not exist."});
+            errors.push(errorEle);
+            Object.assign(result,{errors:errors});
+            newStatusCode = 404;
+
+            //res.status(404)
+            //.send("Resource not found.");
         } else {
-            res.status(response.statusCode)
-            .send(error);
+            let errors=[];
+            let errorEle = {};
+            Object.assign(errorEle,{status: response.statusCode.toString(), title: error});
+            errors.push(errorEle);
+            Object.assign(result,{errors:errors});
+            newStatusCode = response.statusCode;
+            //res.status(response.statusCode)
+            //.send(error);
         }
+        res.status(newStatusCode)
+        .json(result);
       });
 
 }
